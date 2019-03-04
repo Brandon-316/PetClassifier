915e2d568c89d465b604ae26e37a448b7fb352fb
Brandon Mahoney <willb31681@gmail.com>
2019-03-04 08:40:26 -0600

    Initial Commit

--- PetClassifier/ViewController.swift
+++ PetClassifier/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: - Properties
+    let imagePicker: UIImagePickerController = UIImagePickerController()
+    
+    
+    //MARK: - outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    //MARK: - Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else { fatalError("Could not convert UIImage into CIImage.") }
+            
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: PetClassifier().model) else { fatalError("Loading CoreML Model failed.") }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let classification = request.results?.first as? VNClassificationObservation else { fatalError("Model failed to process image.") }
+            
+            self.navigationItem.title = classification.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
     }
 
-
+    
+    //MARK: - Actions
+    @IBAction func barButtonItemTapped(_ sender: UIBarButtonItem) {
+        if sender.tag == 1 {
+            imagePicker.sourceType = .photoLibrary
+        } else {
+            imagePicker.sourceType = .camera
+            imagePicker.showsCameraControls = true
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
